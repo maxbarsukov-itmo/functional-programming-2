b5345c8184ed776edbb196b2c0481ad6811b5e51
maxbarsukov <maximbarsukov@bk.ru>
2024-10-13 19:02:04 +0300

    test: :rotating_light: add some tests

---
+++ test/red_black_tree/node_test.exs
@@ -0,0 +1,23 @@
+defmodule RedBlackTree.NodeTest do
+  @moduledoc """
+  Testing node of Red-Black Tree
+  """
+
+  use ExUnit.Case, async: true
+  alias RedBlackTree.Node
+
+  test "#new" do
+    assert %Node{
+             key: :walrus,
+             value: :bubbles,
+             color: :black,
+             left: nil,
+             right: nil
+           } == Node.new(:walrus, :bubbles)
+  end
+
+  test "#color" do
+    assert :red == Node.color(%Node{color: :black}, :red).color
+    assert :black == Node.color(%Node{color: :red}, :black).color
+  end
+end
